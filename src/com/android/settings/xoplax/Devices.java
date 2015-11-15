fdd42bcd2794c57ec19cacb413944c17337f94a0
Anonim47 <me@yosefadi.xyz>
2015-11-15 10:19:10 +0700

    Settings: Fix Xoplax maintainers list

--- src/com/android/settings/xoplax/Devices.java
+++ src/com/android/settings/xoplax/Devices.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.pac;
+package com.android.settings.xoplax;
 
 import android.os.Bundle;
 import android.content.res.Resources;
